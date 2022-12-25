2a157e5c88ea29a7870a6b1918faa3ee3f06b28c
Pham Huu Hien <phhien203@gmail.com>
2022-12-25 10:24:14 +0700

    add auth guard

---
+++ src/app/auth/services/auth.guard.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from "@angular/router";
+import { OAuthService } from "angular-oauth2-oidc";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private oauthService: OAuthService, private router: Router) {}
+
+  canActivate(
+    _route: ActivatedRouteSnapshot,
+    _state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    if (this.oauthService.hasValidAccessToken()) {
+      return true;
+    }
+
+    return this.router
+      .navigate(["login"])
+      .then(() => {
+        console.log("Route exists, redirection is done");
+        return false;
+      })
+      .catch(() => {
+        console.log("Route not found");
+        this.oauthService.initLoginFlow();
+        return false;
+      });
+  }
+}
